9937ef5ba75cb90648cb3167128724d130423ec5
Jeff Bailey <jeff_bailey@comcast.com>
2023-01-14 10:03:26 -0800

    fix: update to fish v3.6

--- dot_config/fish/functions/fish_prompt.fish
+++ dot_config/fish/functions/fish_prompt.fish
@@ -2,11 +2,11 @@ set -g fish_color_user green
 set -g fish_color_host yellow
 set -g fish_color_cwd normal
 
-set -g __fish_git_prompt_use_informative_chars
-set -g __fish_git_prompt_showdirtystate
-set -g __fish_git_prompt_showuntrackedfiles
-set -g __fish_git_prompt_showupstream verbose
-set -g __fish_git_prompt_showcolorhints
+set -g __fish_git_prompt_use_informative_chars true
+set -g __fish_git_prompt_showdirtystate true
+set -g __fish_git_prompt_showuntrackedfiles true
+set -g __fish_git_prompt_showupstream verbose true
+set -g __fish_git_prompt_showcolorhints true
 set -g __fish_git_prompt_char_untrackedfiles '?'
 set -g __fish_git_prompt_color_upstream green
 set -g __fish_git_prompt_color_branch brcyan
@@ -17,5 +17,5 @@ printf '%s:%s%s %s$%s ' \
     (prompt_pwd -d 3 -D 2) \
     (fish_vcs_prompt) \
     (set_color bryellow) \
-    (set_color normal) 
+    (set_color normal)
 end
