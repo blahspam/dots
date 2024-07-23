c2faa6f445443c2427cf7b6c28315139ef97413b
Jeff Bailey <jeff_bailey@comcast.com>
2024-07-23 14:31:02 -0700

    chore: updates

--- dot_config/docker/cli-plugins/run_link_plugins.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-ln -sfn "$(brew --prefix)/opt/docker-buildx/bin/docker-buildx" "${XDG_CONFIG_HOME}/docker/cli-plugins/docker-buildx"
-ln -sfn "$(brew --prefix)/opt/docker-buildx/bin/docker-compose" "${XDG_CONFIG_HOME}/docker/cli-plugins/docker-compose"
